0b7595b8c5eb8b82f497c008b6ecd2b7a5e0705c
Dietmar Alex Apaza <alexapaza72361453@gmail.com>
2024-09-29 22:38:06 -0400

    Evaluation LAb-05

---
+++ LAB-HCJ/src/hw03/q03.js
@@ -0,0 +1,36 @@
+const rootDiv = document.getElementById("root");
+const root = document.getElementById("root");
+const heading = document.createElement("h2");
+const link = document.createElement("a");
+const buyList = [
+    "books",
+    "bread",
+    "eraser",
+    "earphones",
+    "collection-cards",
+    "hdmi",
+    "vga",
+    "motherboard",
+    "university-books",
+  ];
+  
+  const ul = document.createElement("ul");
+  
+  buyList.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    ul.appendChild(li);
+  });
+link.href = "https://www.google.com";
+link.href = "https://keybr.com";
+
+link.target = "_blank";
+link.textContent = "Google";
+heading.textContent = "USING DOM FOR THE FIRST TIME";
+
+rootDiv.appendChild(link);
+root.appendChild(heading); 
+
+  
+  rootDiv.appendChild(ul);
+  
